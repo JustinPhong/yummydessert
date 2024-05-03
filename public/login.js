422b96d67449faa01bdda11e2d331356bae3e82c
JustinPhong <93855379+JustinPhong@users.noreply.github.com>
2024-05-03 22:41:04 +0000

    trying

--- public/login.js
+++ public/login.js
@@ -2,6 +2,7 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-app.js";
 import { getDatabase, ref, onValue } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-database.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-auth.js";
+import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-auth.js";
 
 
 // Your web app's Firebase configuration

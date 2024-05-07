77e59de01e9c2e284e16f194b74d3394b635196d
JustinPhong <93855379+JustinPhong@users.noreply.github.com>
2024-05-07 19:44:36 +0000

    Added retrieve count from database

--- public/purchase.js
+++ public/purchase.js
@@ -23,115 +23,117 @@ const auth = getAuth();
 
 auth.onAuthStateChanged(function(user) {
     if (user) {
-      const userOrdersRef = ref(db, `users/${user.uid}/orders`);
-      const orderedByDateQuery = query(userOrdersRef, orderByValue("date"));
-  
-      onValue(orderedByDateQuery, (snapshot) => {
-        const orders = snapshot.val();
-        const cardContainer = document.getElementById("card");
-  
-        for (let orderId in orders) {
-          const order = orders[orderId];
-          const { date, menu, subtotal, discount, total } = order;
-          const orderDate = new Date(date);
-          var options = {
-            year: "numeric",
-            month: "short",
-            day: "numeric"
-          };
-          const dayOfWeek = orderDate.toLocaleString('en-AU', options);
-          const timeOfDay = orderDate.toLocaleTimeString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true });
-  
-          const newCard = document.createElement("div");
-          newCard.className = "card";
-          newCard.innerHTML = `
-            <div style="padding: 20px 30px;">
-              <a style="font-weight:bold;">${dayOfWeek}</a>
-              <div style="display:flex;justify-content:space-between;margin:10px 0px 20px;" class="txtfont">
-                <a>${timeOfDay}</a>
-                <div class="ref-container">
-                  <a>ref:</a>
-                  <a>${orderId}</a>
-                </div>
-              </div>`;
-  
-          const userMenusRef = ref(db, `users/${user.uid}/orders/${orderId}/menu`);
-          const promises = [];
-  
-          onValue(userMenusRef, (snapshot) => {
-            const menuIds = snapshot.val();
-            if (menuIds) {
-              for (let id in menuIds) {
-                const menuId = menuIds[id];
-                const menuRef = ref(db, menuId > 10 ? `dessert/${menuId}` : `Menu/0${menuId}`);
-  
-                const promise = new Promise((resolve, reject) => {
-                  onValue(menuRef, (menuSnapshot) => {
-                    const menuData = menuSnapshot.val();
-                    if (menuData) {
-                      const { Image, Name, Price } = menuData;
-                      const menuItem = `
-                        <div style="display:flex;justify-content:space-between;margin: 0px 30px 10px">
-                          <img src="${Image}" class="menuimage">
-                          <div style="display: flex;flex-direction:column;flex:1;margin-left:20px">
-                            <a class="txtfont">${Name}</a>
-                            <div class="txtfont">
-                              <a>x</a>
-                              <a>1</a>
+        const userOrdersRef = ref(db, `users/${user.uid}/orders`);
+        const orderedByDateQuery = query(userOrdersRef, orderByValue("date"));
+
+        onValue(orderedByDateQuery, (snapshot) => {
+            const orders = snapshot.val();
+            const cardContainer = document.getElementById("card");
+
+            for (let orderId in orders) {
+                const order = orders[orderId];
+                const { date, subtotal, discount, total } = order;
+                const orderDate = new Date(date);
+                const dayOfWeek = orderDate.toLocaleString('en-AU', { year: "numeric", month: "short", day: "numeric" });
+                const timeOfDay = orderDate.toLocaleTimeString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true });
+
+                const newCard = document.createElement("div");
+                newCard.className = "card";
+                newCard.innerHTML = `
+                    <div style="padding: 20px 30px;">
+                        <a style="font-weight:bold;">${dayOfWeek}</a>
+                        <div style="display:flex;justify-content:space-between;margin:10px 0px 20px;" class="txtfont">
+                            <a>${timeOfDay}</a>
+                            <div class="ref-container">
+                                <a>ref:</a>
+                                <a>${orderId}</a>
                             </div>
-                          </div>
-                          <div class="txtfont">
-                            <a style="font-weight:bold">RM</a>
-                            <a style="font-weight:bold">${Price}</a>
-                          </div>
                         </div>`;
-                      resolve(menuItem);
-                    } else {
-                      reject(new Error(`Menu with ID ${menuId} not found`));
+
+                const userMenusRef = ref(db, `users/${user.uid}/orders/${orderId}/menu`);
+                const promises = [];
+
+                onValue(userMenusRef, (snapshot) => {
+                    const menuIds = snapshot.val();
+                    if (menuIds) {
+                        for (let id in menuIds) {
+                            const menuId = menuIds[id];
+                            const menuRef = ref(db, menuId > 10 ? `dessert/${menuId}` : `Menu/0${menuId}`);
+                            const promise = new Promise((resolve, reject) => {
+                                onValue(menuRef, (menuSnapshot) => {
+                                    const menuData = menuSnapshot.val();
+                                    if (menuData) {
+                                        const { Image, Name, Price } = menuData;
+
+                                        const userCountsRef = ref(db, `users/${user.uid}/orders/${orderId}/count`);
+                                        onValue(userCountsRef, (userCountsSnapshot) => {
+                                            const userCounts = userCountsSnapshot.val();
+                                            const count = userCounts ? userCounts[id] : 0;
+                                            const newprice = Price * count
+                                            const menuItem = `
+                                                <div style="display:flex;justify-content:space-between;margin: 0px 30px 10px">
+                                                    <img src="${Image}" class="menuimage">
+                                                    <div style="display: flex;flex-direction:column;flex:1;margin-left:20px">
+                                                        <a class="txtfont">${Name}</a>
+                                                        <div class="txtfont">
+                                                            <a>x</a>
+                                                            <a>${count}</a>
+                                                        </div>
+                                                    </div>
+                                                    <div class="txtfont">
+                                                        <a style="font-weight:bold">RM</a>
+                                                        <a style="font-weight:bold">${newprice}</a>
+                                                    </div>
+                                                </div>`;
+                                            resolve(menuItem);
+                                        });
+                                    } else {
+                                        reject(new Error(`Menu with ID ${menuId} not found`));
+                                    }
+                                });
+                            });
+
+                            promises.push(promise);
+                        }
+
+                        Promise.all(promises)
+                            .then((menuItems) => {
+                                menuItems.forEach((menuItem) => {
+                                    newCard.innerHTML += menuItem;
+                                });
+
+                                // Append the completed card to the container
+                                const newCardFooter = `
+                                    <div class="calculate">
+                                        <div style="flex-direction:column;" class="txtfont">
+                                            <div class="line"></div>
+                                            <div style="display: flex; justify-content: space-between;margin-bottom:20px">
+                                                <a style="margin-right:40px">Subtotal</a>
+                                                <a>${subtotal}</a>
+                                            </div>
+                                            <div style="display: flex; justify-content: space-between;margin-bottom:20px">
+                                                <a style="margin-right:40px">Discount</a>
+                                                <a>${discount}</a>
+                                            </div>
+                                            <div class="line"></div>
+                                            <div style="display: flex; justify-content: space-between;margin-bottom:20px">
+                                                <a style="margin-right:40px">Total</a>
+                                                <a>${total}</a>
+                                            </div>
+                                        </div>
+                                    </div>`;
+
+                                newCard.innerHTML += newCardFooter;
+                                cardContainer.appendChild(newCard);
+                            })
+                            .catch((error) => {
+                                console.error("Error fetching menu items:", error);
+                            });
                     }
-                  });
                 });
-  
-                promises.push(promise);
-              }
-  
-              Promise.all(promises).then((menuItems) => {
-                menuItems.forEach((menuItem) => {
-                  newCard.innerHTML += menuItem;
-                });
-  
-                // Append the completed card to the container
-                const newCardFooter = `
-                  <div class="calculate">
-                    <div style="flex-direction:column;" class="txtfont">
-                      <div class="line"></div>
-                      <div style="display: flex; justify-content: space-between;margin-bottom:20px">
-                        <a style="margin-right:40px">Subtotal</a>
-                        <a>RM ${subtotal}</a>
-                      </div>
-                      <div style="display: flex; justify-content: space-between;margin-bottom:20px">
-                        <a style="margin-right:40px">Discount</a>
-                        <a>RM ${discount}</a>
-                      </div>
-                      <div class="line"></div>
-                      <div style="display: flex; justify-content: space-between;margin-bottom:20px">
-                        <a style="margin-right:40px">Total</a>
-                        <a>RM ${total}</a>
-                      </div>
-                    </div>
-                  </div>`;
-  
-                newCard.innerHTML += newCardFooter;
-                cardContainer.appendChild(newCard);
-              }).catch((error) => {
-                console.error("Error fetching menu items:", error);
-              });
             }
-          });
-        }
-      }, (error) => {
-        console.error("Error fetching user orders:", error);
-      });
+        }, (error) => {
+            console.error("Error fetching user orders:", error);
+        });
     }
-  });
-  
+});

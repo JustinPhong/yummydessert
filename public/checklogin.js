a5ee3a1b48514c45d78ac334cf33fea2f676e608
Justin Phong <justinphong@Justins-MacBook-Air-85.local>
2024-07-01 16:02:46 +0800

    Account button

--- public/checklogin.js
+++ public/checklogin.js
@@ -1,40 +1,38 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-auth.js";
-
+import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-auth.js";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-apiKey: "AIzaSyACZZIyq7T1sU3TtvFJI5FKkAXPYZ4yf8I",
-authDomain: "finalproject-8f27c.firebaseapp.com",
-databaseURL: "https://finalproject-8f27c-default-rtdb.asia-southeast1.firebasedatabase.app",
-projectId: "finalproject-8f27c",
-storageBucket: "finalproject-8f27c.appspot.com",
-messagingSenderId: "644459011945",
-appId: "1:644459011945:web:6d4a2d7d2fe00c53a027f8",
-measurementId: "G-HY8X68GH3R"
+    apiKey: "AIzaSyACZZIyq7T1sU3TtvFJI5FKkAXPYZ4yf8I",
+    authDomain: "finalproject-8f27c.firebaseapp.com",
+    databaseURL: "https://finalproject-8f27c-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "finalproject-8f27c",
+    storageBucket: "finalproject-8f27c.appspot.com",
+    messagingSenderId: "644459011945",
+    appId: "1:644459011945:web:6d4a2d7d2fe00c53a027f8",
+    measurementId: "G-HY8X68GH3R"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 
+// Add event listener to the account button
 document.getElementById("accountbtn").addEventListener("click", () => {
     const user = auth.currentUser;
-        if (!user){
-            window.location.href="login.html";
-            return;
-        } else {
-            window.location.href = "account.html";
-            return;
-        }
+    if (!user) {
+        window.location.href = "login.html";
+    } else {
+        window.location.href = "account.html";
+    }
 });
 
-document.addEventListener('DOMContentLoaded', function() {
-    const user = auth.currentUser;
-    if (!user){
-        document.getElementById("accountbtn").textContent = "Login"
+// Monitor authentication state
+onAuthStateChanged(auth, (user) => {
+    if (user) {
+        document.getElementById("accountbtn").textContent = "Account";
     } else {
-        document.getElementById("accountbtn").textContent = "Account"
+        document.getElementById("accountbtn").textContent = "Login";
     }
-}, false);
+});
